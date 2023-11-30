be7cfdfc31af5aafec5335eba6e91766dde86ba4
nhatminhtrieu <21127112@student.hcmus.edu.vn>
2023-11-30 22:06:00 +0700

    AMR-1: - update: gitignore to access .env - added: config/index.js

---
+++ schema/config/index.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+
+export const PORT = process.env.PORT;
+export const CONNTECTION_STRING = process.env.CONNECTION_STRING;
+export const JWT_SECRET = process.env.JWT_SECRET;
