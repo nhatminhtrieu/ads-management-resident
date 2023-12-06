02510920902a67862fba5ceac25334a4bede4705
Ngan Truc <ngantruc2003@gmail.com>
2023-12-06 13:33:17 +0700

    add info window

--- static/js/map/initMap.js
+++ static/js/map/initMap.js
@@ -1,18 +1,26 @@
 import initCurrentLocation from "./currentLocation.js";
 
 import moveToCurrenLocation from "../service/moveToCurrentLocation.js";
+<<<<<<< Updated upstream
 import toggleContainer  from "../service/Toggle.js";
+=======
+import toggleContainer from "../service/Toggle.js";
+>>>>>>> Stashed changes
 
 function handleStatus(status) {
   //[switchAds, switchReports]
   //call again function to hide or show ads and reports
 }
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 async function initMap() {
   const map = await initCurrentLocation();
   moveToCurrenLocation(map);
 
+<<<<<<< Updated upstream
   let status = [false, false]
   toggleContainer(map, (switchAds, switchReports) => {
     status = [switchAds, switchReports]
@@ -21,3 +29,13 @@ async function initMap() {
 }
 
 initMap();
+=======
+  let status = [false, false];
+  toggleContainer(map, (switchAds, switchReports) => {
+    status = [switchAds, switchReports];
+    handleStatus(status);
+  });
+}
+
+initMap();
+>>>>>>> Stashed changes
