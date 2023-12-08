a27e5f48a18cb5deb3919e9b3947dd273b6042dd
nhatminhtrieu <21127112@student.hcmus.edu.vn>
2023-12-08 15:08:37 +0700

    AMR-1 update advertisement schema (add numberOfBoard property)

--- repositories/AdvertisementRepository.js
+++ repositories/AdvertisementRepository.js
@@ -1,22 +1,8 @@
 import AdvertisementModel from "../models/Advertisement.js";
 
 class AdvertisementRepository {
-  constructor() {
-    this.model = AdvertisementModel;
-  }
   async createAdvertisement(advertisement) {
     const newAdvertisement = new AdvertisementModel(advertisement);
     return await newAdvertisement.save();
   }
-
-  async getAllAdvertisements() {
-    try {
-      return await this.model.find({});
-    } catch (err) {
-      console.err("getAllAdvertisements", err);
-      throw err;
-    }
-  }
 }
-
-export default AdvertisementRepository;
