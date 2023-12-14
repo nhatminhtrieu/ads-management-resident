d7dbeb4e3633852de4df3436e41ef38543aa101e
nhatminhtrieu <21127112@student.hcmus.edu.vn>
2023-12-14 10:31:45 +0700

    AMR-1 add number property

--- models/Advertisement.js
+++ models/Advertisement.js
@@ -9,55 +9,57 @@ const AdvertisementSchema = new Schema({
     address_components: [
       {
         type: Object,
-        required: true
       },
     ],
     formatted_text: {
       type: String,
-      required: true
+      required: true,
     },
   },
   coordinate: {
     type: Object,
-    required: true
+    required: true,
   },
   typeLoc: {
-    type: [String],
-    enum: ['PublicLand', 'Park', 'TrafficSafetyCorridor', 'PrivateLand', 'IndividualHouse', 'CommercialCenter', 'Market', 'GasStation', 'BusStation'],
-    required: true
+    type: String,
+    required: true,
   },
   typeAds: {
     type: String,
-    enum: ['PoliticalPromotion', 'CommercialPromotion', 'PublicServiceAnnouncement', 'EventPromotion', 'ProductLaunch'],
-    required: true
+    required: true,
   },
   typeBoard: {
     type: String,
-    enum: ['HiflexPanelPosts', 'LEDElectronicDisplayPosts', 'LightboxPillar', 'WallCladdingHiflexBoard', 'WallCladdingElectronicDisplay', 'VerticalBannerHangingPosts', 'HorizontalBannerHangingPosts', 'PillarsPanelAssemblies', 'GreetingPort', 'ShoppingMalls'],
-    required: true
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
   },
   zoning: {
     type: Boolean,
-    required: true
+    required: true,
   },
   size: {
     type: String,
-    required: true
+    required: true,
   },
-  imgs: [{
-    type: Schema.Types.ObjectId,
-    ref: 'Image',
-    required: true
-  }],
+  imgs: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Image",
+      required: true,
+    },
+  ],
   exp: {
     type: Date,
-    required: true
+    required: true,
   },
   status: {
     type: String,
-    enum: ['draft', 'pending', 'active', 'expired', 'suspended'],
-    required: true
-  }
+    enum: ["draft", "pending", "active", "expired", "suspended"],
+    required: true,
+  },
 });
 
 const Advertisement = model(
