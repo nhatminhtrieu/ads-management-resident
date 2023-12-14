c37227e445318694bb2665348fffc93cec8b64db
nhatminhtrieu <21127112@student.hcmus.edu.vn>
2023-12-14 10:18:56 +0700

    AMR-1 resolved conflict

--- models/Advertisement.js
+++ models/Advertisement.js
@@ -11,47 +11,53 @@ const AdvertisementSchema = new Schema({
         type: Object,
         required: true
       },
-      coordinate: {
-        type: Object,
-        required: true
-      },
-      typeLoc: {
-        type: [String],
-        enum: ['PublicLand', 'Park', 'TrafficSafetyCorridor', 'PrivateLand', 'IndividualHouse', 'CommercialCenter', 'Market', 'GasStation', 'BusStation'],
-        required: true
-      },
-      typeAds: {
-        type: String,
-        enum: ['PoliticalPromotion', 'CommercialPromotion', 'PublicServiceAnnouncement', 'EventPromotion', 'ProductLaunch'],
-        required: true
-      },
-      typeBoard: {
-        type: String,
-        enum: ['HiflexPanelPosts', 'LEDElectronicDisplayPosts', 'LightboxPillar', 'WallCladdingHiflexBoard', 'WallCladdingElectronicDisplay', 'VerticalBannerHangingPosts', 'HorizontalBannerHangingPosts', 'PillarsPanelAssemblies', 'GreetingPort', 'ShoppingMalls'],
-        required: true
-      },
-      zoning: {
-        type: Boolean,
-        required: true
-      },
-      size: {
-        type: String,
-        required: true
-      },
-      imgs: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Image',
-        required: true
-      }],
-      exp: {
-        type: Date,
-        required: true
-      },
-      status: {
-        type: String,
-        enum: ['draft', 'pending', 'active', 'expired', 'suspended'],
-        required: true
-      }
+    ],
+    formatted_text: {
+      type: String,
+      required: true
+    },
+  },
+  coordinate: {
+    type: Object,
+    required: true
+  },
+  typeLoc: {
+    type: [String],
+    enum: ['PublicLand', 'Park', 'TrafficSafetyCorridor', 'PrivateLand', 'IndividualHouse', 'CommercialCenter', 'Market', 'GasStation', 'BusStation'],
+    required: true
+  },
+  typeAds: {
+    type: String,
+    enum: ['PoliticalPromotion', 'CommercialPromotion', 'PublicServiceAnnouncement', 'EventPromotion', 'ProductLaunch'],
+    required: true
+  },
+  typeBoard: {
+    type: String,
+    enum: ['HiflexPanelPosts', 'LEDElectronicDisplayPosts', 'LightboxPillar', 'WallCladdingHiflexBoard', 'WallCladdingElectronicDisplay', 'VerticalBannerHangingPosts', 'HorizontalBannerHangingPosts', 'PillarsPanelAssemblies', 'GreetingPort', 'ShoppingMalls'],
+    required: true
+  },
+  zoning: {
+    type: Boolean,
+    required: true
+  },
+  size: {
+    type: String,
+    required: true
+  },
+  imgs: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Image',
+    required: true
+  }],
+  exp: {
+    type: Date,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['draft', 'pending', 'active', 'expired', 'suspended'],
+    required: true
+  }
 });
 
 const Advertisement = model(
