5780139042c409c6abff34b00320b133947c2270
Ngan Truc <ngantruc2003@gmail.com>
2023-12-27 17:08:29 +0700

    finish fix bug in clicking report marker

--- static/js/main.js
+++ static/js/main.js
@@ -7,28 +7,28 @@ import SideBar from "./service/SideBar.js";
 // toggle is false means the side bar is being hidden
 var toggle = false;
 async function main() {
-  const map = new IMap();
-  await map.initMap();
+	const map = new IMap();
+	await map.initMap();
 
-  const service = new Service(map);
+	const service = new Service(map);
 
-  const sideBar = new SideBar();
-  sideBar.init(map);
+	const sideBar = new SideBar();
+	sideBar.init(map);
 
-  service.moveToCurrentLocation();
-  service.showAllMarker();
-  service.preloadCaptcha();
-  await loadMarker(map);
-  await service.loadReportMarkers();
-  service.clusterMarkers();
+	service.moveToCurrentLocation();
+	service.showAllMarker();
+	service.preloadCaptcha();
+	await loadMarker(map);
+	service.clusterMarkers();
+	await service.loadReportMarkers();
 
-  const form = new Form(map);
-  service.catchUserSelectedLocation(form);
-  service.catchUserClickMarker(form);
-  form.resetFormFields();
-  const captcha = await service.verifyCaptcha();
-  form.setCaptcha(captcha);
-  form.catchUserSubmitReport();
+	const form = new Form(map);
+	service.catchUserSelectedLocation(form);
+	service.catchUserClickMarker(form);
+	form.resetFormFields();
+	const captcha = await service.verifyCaptcha();
+	form.setCaptcha(captcha);
+	form.catchUserSubmitReport();
 }
 
 main();
